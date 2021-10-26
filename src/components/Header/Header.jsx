34570daf4ce6bc0fc5450785c09d3a6d8150dff6
Camilo Vargas <camilovargasingm@gmail.com>
2021-10-26 18:06:51 -0500

    correction menuResponsive and transition

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,15 +2,17 @@ import React, {useState} from 'react'
 import "./Header.scss"
 import {Link} from "react-router-dom"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faBars, faSlash } from '@fortawesome/free-solid-svg-icons'
+import { faBars, faTimes } from '@fortawesome/free-solid-svg-icons'
 
 export default function Header() {
-  const [isMobile, setIsMobile] = useState(true)
+  const [isMobile, setIsMobile] = useState(false)
   return (
     <header className="header container">
       <h1 className="header_logo">myLorem</h1>
       <nav className="navbar">
-        <ul className={isMobile? "header_link mobile" : "header_link"}>
+        <ul className={isMobile? "header_link mobile" : "header_link"}
+          onClick={() => setIsMobile(!isMobile)}
+          >
           <li>
             <Link to="/">welcome</Link>
           </li>
@@ -28,33 +30,11 @@ export default function Header() {
           </li>
         </ul>
       </nav>
-      <button className="mobile_menu_icon">
-        {isMobile? <FontAwesomeIcon icon={faBars}/> : <FontAwesomeIcon icon={faSlash}/> }
+      <button className="mobile_menu_icon"
+        onClick={() => setIsMobile(!isMobile)}
+        >
+        {isMobile? <FontAwesomeIcon icon={faTimes}/> : <FontAwesomeIcon icon={faBars}/> }
       </button>
     </header>
-  )
-}
-
-function MenuTopWeb(){
-  return(
-    <>
-      <ul className="header_menutop">
-        <li>
-          <Link to="/">welcome</Link>
-        </li>
-        <li>
-          <Link to="/">Dropdown</Link>
-        </li>
-        <li>
-          <Link to="/">left sidebar</Link>
-        </li>
-        <li>
-          <Link to="/">right sidebar</Link>
-        </li>
-        <li>
-          <Link to="/">no sidebar</Link>
-        </li>
-      </ul>
-    </>
   )
 }
