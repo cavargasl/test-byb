8d3d6832cc4c5d8ca196ebbf69add7aad765b875
Camilo Vargas <camilovargasingm@gmail.com>
2021-10-26 23:47:24 -0500

    add active button

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,19 @@
 import React, {useState} from 'react'
 import "./Header.scss"
-import {Link} from "react-router-dom"
+import {Link, useHistory} from "react-router-dom"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faBars, faTimes } from '@fortawesome/free-solid-svg-icons'
 
 export default function Header() {
   const [isMobile, setIsMobile] = useState(false)
+  const history = useHistory()
+  const [active, setActive] = useState(false)
+  const changeActive = () => {
+    if(active === history.location.pathname){
+      return setActive(false)
+    }
+    return setActive(history.location.pathname)
+  }
   return (
     <header className="header container">
       <h1 className="header_logo">myLorem</h1>
@@ -13,19 +21,19 @@ export default function Header() {
         <ul className={isMobile? "header_link mobile" : "header_link"}
           onClick={() => setIsMobile(!isMobile)}
           >
-          <li>
+          <li className={active === "/" ? "active" : "" } onClick={() => changeActive()}>
             <Link to="/">welcome</Link>
           </li>
-          <li>
+          <li className={active === "/dropdown" ? "active" : "" } onClick={() => changeActive()}>
             <Link to="/dropdown">Dropdown</Link>
           </li>
-          <li>
+          <li className={active === "/left_sidebar" ? "active" : "" } onClick={() => changeActive()}>
             <Link to="/left_sidebar">left sidebar</Link>
           </li>
-          <li>
+          <li className={active === "/right_sidebar" ? "active" : "" } onClick={() => changeActive()}>
             <Link to="/right_sidebar">right sidebar</Link>
           </li>
-          <li>
+          <li className={active === "/no_sidebar" ? "active" : "" } onClick={() => changeActive()}>
             <Link to="/no_sidebar">no sidebar</Link>
           </li>
         </ul>
