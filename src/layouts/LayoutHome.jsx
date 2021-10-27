14024c37b1e5da1aa813207ff58419a49e83099e
Camilo Vargas <camilovargasingm@gmail.com>
2021-10-26 20:15:31 -0500

    add component Footer in the layoutHome

--- src/layouts/LayoutHome.jsx
+++ src/layouts/LayoutHome.jsx
@@ -2,13 +2,14 @@ import React from 'react'
 import "./LayoutHome.scss"
 
 import Header from '../components/Header/Header'
+import Footer from '../components/Footer/Footer'
 
 export default function LayoutHome({children}) {
   return (
     <div className="main">
       <Header />
       {children}
-      <p>footer</p>
+      <Footer />
     </div>
   )
 }
