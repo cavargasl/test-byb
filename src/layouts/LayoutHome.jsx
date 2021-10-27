9d9aab8120f669fa94ae2266f19d06d5582254da
Camilo Vargas <camilovargasingm@gmail.com>
2021-10-26 21:13:19 -0500

    delete code unnesesary of layoutHome

--- src/layouts/LayoutHome.jsx
+++ src/layouts/LayoutHome.jsx
@@ -1,12 +1,11 @@
 import React from 'react'
-import "./LayoutHome.scss"
 
 import Header from '../components/Header/Header'
 import Footer from '../components/Footer/Footer'
 
 export default function LayoutHome({children}) {
   return (
-    <div className="main">
+    <div>
       <Header />
       {children}
       <Footer />
